17e6cbcc0a58caed2b4e1b1e2dd0cc84637de4b1
Kei <kei24west@gmail.com>
2021-09-10 00:26:59 +0900

    first files (#1)
    * add setup.sh
    
    * add .vimrc
    
    * add .zshrc
    
    * add submodule molokai

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo xtrace
+
+BASEDIR=$(dirname $0)
+cd ${BASEDIR}
+
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+
+    ln -snfv ${PWD}/"$f" ~/
+done
+
+git clone https://github.com/tomasr/molokai ${HOME}/.vim
